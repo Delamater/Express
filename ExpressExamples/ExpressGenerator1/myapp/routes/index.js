aa6591fe3d4c03fa5ece8ab385e483cee6ab715d
Delamater <Bob.Delamater@sage.com>
2018-11-14 12:19:57 -0800

    Simple directory check and response

--- ExpressExamples/ExpressGenerator1/myapp/routes/index.js
+++ ExpressExamples/ExpressGenerator1/myapp/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/bob/', function(req, res, next) {
+  res.render('index', { title: 'Bob' });
+  console.log("This is my console log");
+})
+
 module.exports = router;
